b87d192ea89ef0587910041a048395276bd000ca
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2023-01-24 03:45:37 +0600

    About & Experience section done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,62 @@
 import React from "react";
 import "./About.css";
+import ME from "../../Assets/pro.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
     <section id="about">
-      <h1>About</h1>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me_image">
+            <img src={ME} alt="Me" />
+          </div>
+        </div>
+
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon"></FaAward>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about_icon"></FiUsers>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon"></VscFolderLibrary>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Hello, This is Ashik Ahammed Hridoy. I am a MERN Stack Developer &
+            Ethical Hacker. I have 3 years of experience. I study computer
+            science and engineering. I am very passionate about what I do. I
+            offer Front-End & Full Stack Mern Web Applications development using
+            HTML, CSS, Bootstrap, Tailwind CSS, JavaScript, React JS, Node JS,
+            Express JS, MongoDB, Firebase, and JWT. I also provide fully
+            responsive WordPress website design & development. I am also an
+            expert in Ethical Hacking. I offer web application penetration
+            testing and WordPress website security. Thanks for your valuable
+            time
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,103 @@
 import React from "react";
 import "./Experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
     <section id="experience">
-      <h1>Experience</h1>
+      <h5>My Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience_container">
+        <div className="experience_frontend">
+          <h3>Front-End Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>React JS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience_backend">
+          <h3>Back-End Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>Node JS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>Firebase</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience_details">
+              <BsPatchCheckFill className="experience_details_icon"></BsPatchCheckFill>
+              <div>
+                <h4>JWT</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
